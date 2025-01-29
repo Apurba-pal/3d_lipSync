ff394e2df9ce1fef13f9f717af084ec4e0e5a83c
Apurba <palapurba2004@gmail.com>
2025-01-29 15:35:39 +0530

    created a box room

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,17 +3,44 @@ import { useThree } from "@react-three/fiber";
 import { Avatar } from "./Avatar";
 
 export const Experience = () => {
-  const texture = useTexture("textures/youtubeBackground.jpg");
+  const texture = useTexture("textures/gradient_akatsuki.png");
   const viewport = useThree((state) => state.viewport);
 
   return (
     <>
       <OrbitControls />
-      <Avatar position={[0, -3, 5]} scale={2} />
+      <Avatar position={[0, -2, 0]} scale={2} />
       <Environment preset="sunset" />
       <mesh>
-        <planeGeometry args={[viewport.width, viewport.height]} />
-        <meshBasicMaterial map={texture} />
+        {/* <planeGeometry args={[viewport.width, viewport.height]} /> */}
+        {/* <planeGeometry args={[viewport.width, viewport.height]} rotateX={90} /> */}
+        {/* <meshBasicMaterial map={texture} /> */}
+        {/* <meshBasicMaterial color="blue" /> */}
+      </mesh>
+      <mesh position={[0, 0, -5]}>
+        <boxGeometry args={[10, 10, 0.1]} />
+        <meshBasicMaterial color="blue" map={texture}/>
+      </mesh>
+      <mesh position={[0, 0, 5]}>
+        {/* <boxGeometry args={[1, 1, 1]} /> */}
+        {/* <planeGeometry args={[10,10]}/> */}
+        <meshBasicMaterial color="blue" transparent opacity={0} />
+      </mesh>
+      <mesh position={[-5, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
+        <boxGeometry args={[10, 10, 0.1]} />
+        <meshBasicMaterial color="blue" map={texture}/>
+      </mesh>
+      <mesh position={[5, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
+        <boxGeometry args={[10, 10, 0.1]} />
+        <meshBasicMaterial color="blue" map={texture}/>
+      </mesh>
+      <mesh position={[0, 5, 0]} rotation={[Math.PI / 2, 0, 0]}>
+        <boxGeometry args={[10, 10, 0.1]} />
+        <meshBasicMaterial color="blue" map={texture}/>
+      </mesh>
+      <mesh position={[0, -5, 0]} rotation={[Math.PI / 2, 0, 0]}>
+        <boxGeometry args={[10, 10, 0.1]} />
+        <meshBasicMaterial color="blue" map={texture}/>
       </mesh>
     </>
   );
